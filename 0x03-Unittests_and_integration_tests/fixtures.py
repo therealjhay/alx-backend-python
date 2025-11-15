e3441311fb4e69729a55df4c6c90f8c6f9ccb7f5
Johnson Oyemade <johnsonoca@gmail.com>
2025-11-15 11:28:56 +0100

    fixtures update

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
+org_payload = {
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
+
+repos_payload = [
+    {"name": "episodes.dart", "license": {"key": "bsd-3-clause"}},
+    {"name": "cpp-netlib", "license": {"key": "bsl-1.0"}},
+    {"name": "dagger", "license": {"key": "apache-2.0"}},
+    {"name": "ios-webkit-debug-proxy", "license": {"key": "other"}},
+    {"name": "google.github.io", "license": None},
+    {"name": "kratu", "license": {"key": "apache-2.0"}},
+    {"name": "build-debian-cloud", "license": {"key": "other"}},
+    {"name": "traceur-compiler", "license": {"key": "apache-2.0"}},
+    {"name": "firmata.py", "license": {"key": "apache-2.0"}},
+]
+
+expected_repos = [
+    "episodes.dart",
+    "cpp-netlib",
+    "dagger",
+    "ios-webkit-debug-proxy",
+    "google.github.io",
+    "kratu",
+    "build-debian-cloud",
+    "traceur-compiler",
+    "firmata.py",
+]
+
+apache2_repos = [
+    "dagger",
+    "kratu",
+    "traceur-compiler",
+    "firmata.py",
+]
 
 TEST_PAYLOAD = [
   (
